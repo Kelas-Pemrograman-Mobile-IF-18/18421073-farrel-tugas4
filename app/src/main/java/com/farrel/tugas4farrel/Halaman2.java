80c1f71afa79afa7a91df35d3b95b3cd903141ef
Yollandha <yollandhaaa@gmail.com>
2021-04-28 01:25:34 +0700

    Tugas4

---
+++ app/src/main/java/com/farrel/tugas4farrel/Halaman2.java
@@ -0,0 +1,26 @@
+package com.farrel.tugas4farrel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Halaman2 extends AppCompatActivity {
+
+    @BindView(R.id.txtNama)
+    TextView txtNama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman2);
+
+        ButterKnife.bind(this);
+
+        Intent i = getIntent();
+        String nama = i.getStringExtra("nama");
+        txtNama.setText(nama);
+    }
+}
